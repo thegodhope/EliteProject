0a72cf39580dfbe232b56e6a74d752edc58e9585
thegodhope <103210115+thegodhope@users.noreply.github.com>
2024-03-19 20:01:57 +0100

    added cors

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const userController = require("./controllers/userController");
 const app = express();
 require("./database/connection");
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
+app.use(cors());
 
 // Routes
 app.post("/submit_message", userController.submitMessage);
