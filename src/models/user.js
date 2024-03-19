32c8b5604dbe3d5600901a334878ac9e47282876
thegodhope <103210115+thegodhope@users.noreply.github.com>
2024-03-19 21:30:45 +0100

    updated schema

--- src/models/user.js
+++ src/models/user.js
@@ -1,11 +1,11 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    username: { type: String, required: true },
-    phone_number: { type: String },
-    email: { type: String},
-    message: { type: String },
-    created_at: { type: Date, default: Date.now }
+  username: { type: String },
+  phone_number: { type: String },
+  email: { type: String },
+  message: { type: String },
+  created_at: { type: Date, default: Date.now },
 });
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
